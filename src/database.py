74bd590529acd3b0474e2cc80b170e31d1310220
Sahil Lotya <sahillotya021@gmail.com>
2022-09-01 10:49:33 +0530

    commiting

--- src/database.py
+++ src/database.py
@@ -11,18 +11,18 @@ def list_reminders():
                 print(e.ljust(32), end=' ')
         print()
 
-def add_reminder(reminder):
-    print()
-    reminder = input("What would you like to be reminded about?: ")
+# def add_reminder(reminder):
+#     print()
+#     reminder = input("What would you like to be reminded about?: ")
 
-def add_reminder(text, date, ReminderClass):
-    reminder = ReminderClass(text, date)
+# def add_reminder(text, date, ReminderClass):
+#     reminder = ReminderClass(text, date)
 
-    if not isinstance(reminder, DeadlinedReminder):
-        raise TypeError('Invalid Reminder Class')
+#     if not isinstance(reminder, DeadlinedReminder):
+#         raise TypeError('Invalid Reminder Class')
 
 
-    with open('reminders.csv', 'a+', newline='\n') as file:
-        writer = csv.writer(file)
-        writer.writerow([reminder]) 
-        writer.writerow(reminder)
+#     with open('reminders.csv', 'a+', newline='\n') as file:
+#         writer = csv.writer(file)
+#         writer.writerow([reminder]) 
+#         writer.writerow(reminder)
